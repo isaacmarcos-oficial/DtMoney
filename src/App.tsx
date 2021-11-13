134ebdb47875d4f9059c30e63509f7353a1c382b
Isaac Marcos <isaacmarcos@scdesign.com.br>
2021-11-13 16:58:37 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import Modal from 'react-modal'
+import { useState } from 'react';
+import { Dashboard } from "./components/Dashboard";
+import { Header } from "./components/Header";
+import { GlobalStyle } from "./styles/global";
+import { TransactionsProvider } from './hooks/useTransactions';
+import { NewTransactionModal } from './components/NewTransactionModal';
+
+Modal.setAppElement('#root');
+
+
+export function App() {
+  const [ isNewTransactionModalOpen, setIsNewTransactionModalOpen ] = useState(false);
+  
+  function handleOpenNewTransactionModal() {
+      setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+      setIsNewTransactionModalOpen(false);
+  }
+
+  return (
+    <TransactionsProvider>
+        <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
+
+        <Dashboard/>
+
+        <NewTransactionModal
+          isOpen={isNewTransactionModalOpen}
+          onRequestClose={handleCloseNewTransactionModal}
+        />
+        
+        <GlobalStyle/>
+    </TransactionsProvider>
+  );
+}
